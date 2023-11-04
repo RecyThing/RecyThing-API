7d06b22894524b4b2dd4a1358a9a91da7cd7ebd6
teranixbq <hanieffathulb03@gmail.com>
2023-11-04 19:58:25 +0700

    feat: add response struct

---
+++ utils/helper/response_http.go
@@ -0,0 +1,29 @@
+package helper
+
+type ErrorResponseJson struct {
+	Message string `json:"message"`
+}
+
+type SuccessResponseJson struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func FailedResponse(message string) ErrorResponseJson {
+	return ErrorResponseJson{
+		Message: message,
+	}
+}
+
+func SuccessResponse(message string) SuccessResponseJson {
+	return SuccessResponseJson{
+		Message: message,
+	}
+}
+
+func SuccessWithDataResponse(message string, data interface{}) SuccessResponseJson {
+	return SuccessResponseJson{
+		Message: message,
+		Data:    data,
+	}
+}

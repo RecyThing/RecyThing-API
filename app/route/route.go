be272103fba058e373d17e759a042af9f081d4ab
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-11-11 16:59:52 +0800

    feat:remove admin

--- app/route/route.go
+++ app/route/route.go
@@ -6,11 +6,11 @@ import (
 )
 
 func New(e *echo.Echo, db *gorm.DB) {
-	admin := e.Group("/admins")
+	
 	user := e.Group("/users")
 	report := e.Group("/reports")
 	recybot := e.Group("/admins")
-	RouteAdmin(admin, db)
+	
 	RouteUser(user, db)
 	RouteReport(report, db)
 	RouteRecybot(recybot, db)

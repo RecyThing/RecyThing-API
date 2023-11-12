d4881b971ec26fbc27279758898cf1ca8db30f39
teranixbq <hanieffathulb03@gmail.com>
2023-11-12 23:48:53 +0700

    feat: add custom regex valid phone number

--- utils/helper/constanta.go
+++ utils/helper/constanta.go
@@ -2,6 +2,7 @@ package helper
 
 const (
 	VERIFICATION_URL = "http://localhost:8080/verify-token?token="
+
 	USER             = "user"
 	ADMIN            = "admin"
 	SUPERADMIN       = "super_admin"

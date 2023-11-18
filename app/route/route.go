1db030bad546fe2b96ae3868ff2d09bf6823fd69
Stanley <stanleyjo41@gmail.com>
2023-11-18 22:34:14 +0700

    feat:add get all faq and get faq by id

--- app/route/route.go
+++ app/route/route.go
@@ -10,9 +10,11 @@ func New(e *echo.Echo, db *gorm.DB) {
 	user := e.Group("/users")
 	report := e.Group("/reports")
 	admin:=e.Group("/admins")
+	faq:=e.Group("/faq")
 
 	RouteLoginPage(loginPage,db)
 	RouteUser(user, db)
 	RouteReport(report, db)
 	RouteAdmin(admin, db)
+	RouteFaqs(faq, db)
 }
